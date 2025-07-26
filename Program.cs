f1bfe2a7fb8187ea0a37e03a35c654385a2c4370
Thiago Marques <thiago77mar@gmail.com>
2025-07-26 14:36:32 -0300

    Primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "4589", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.Ligar();
+nokia.InstalarAplicativo("Telegram");
